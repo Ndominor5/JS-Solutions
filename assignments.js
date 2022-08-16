1f63d9bbcae221060f4aae975b1e490a43f0c15d
Ndominor5 <reyesminorf@guilford.edu>
2022-08-16 18:47:23 -0500

    Adding a new file

---
+++ assignments.js
@@ -0,0 +1,75 @@
+/*
+// Values and variables & Let, const and var
+const country = "Mexico";
+const continent = "America";
+let population = 120000000;
+
+console.log(country, continent, population);
+
+// Data types
+let isIsland = false;
+let language;
+
+console.log(
+  typeof isIsland,
+  typeof population,
+  typeof country,
+  typeof language
+);
+
+// let, const and ver
+language = "Spanish";
+console.log(language);
+
+// continent = "Europe";
+
+// basic operators
+let halfPopulation = population / 2;
+console.log(halfPopulation);
+
+population++;
+console.log(population);
+
+let finlandPopulation = 6000000;
+console.log(population > finlandPopulation);
+
+let description =
+  country +
+  " " +
+  "is in " +
+  continent +
+  " " +
+  "and its " +
+  population +
+  " " +
+  "speaks " +
+  language +
+  ".";
+console.log(description);
+
+// Coding challenge #1
+let markMass = 78;
+let johnMass = 92;
+let markHeight = 1.69;
+let johnHeight = 1.95;
+
+let markBMI = markMass / markHeight ** 2;
+let johnBMI = johnMass / johnHeight ** 2;
+
+console.log(markBMI, johnBMI);
+
+let markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+
+markMass = 95;
+johnMass = 85;
+markHeight = 1.88;
+johnHeight = 1.76;
+markBMI = markMass / markHeight ** 2;
+johnBMI = johnMass / johnHeight ** 2;
+
+console.log(markBMI, johnBMI);
+
+markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+*/
