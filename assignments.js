2e16e52c4838e207c12513193fbe7409719587f1
Ndominor5 <reyesminorf@guilford.edu>
2022-08-27 18:13:26 -0500

    Adding more modules up to 22.

--- assignments.js
+++ assignments.js
@@ -1,5 +1,5 @@
 /*
-// Values and variables & Let, const and var
+// 13 Values and variables & Let, const and var
 const country = "Mexico";
 const continent = "America";
 let population = 120000000;
@@ -23,7 +23,7 @@ console.log(language);
 
 // continent = "Europe";
 
-// basic operators
+// 14 basic operators
 let halfPopulation = population / 2;
 console.log(halfPopulation);
 
@@ -72,4 +72,50 @@ console.log(markBMI, johnBMI);
 
 markHigherBMI = markBMI > johnBMI;
 console.log(markHigherBMI);
+
+//17. String and Template Literals
+const country = "Mexico";
+const continent = "America";
+const population = 120000000;
+const language = "Spanish";
+
+console.log(
+  `${country} is in ${continent}, and its ${population} million people speak ${language}.`
+);
 */
+
+// Coding Challenge #2
+
+let markMass = 95;
+let markHeight = 1.88;
+let johnMass = 85;
+let johnHeight = 1.76;
+
+let markBMI = markMass / markHeight ** 2;
+let johnBMI = johnMass / johnHeight ** 2;
+
+console.log(markBMI, johnBMI);
+
+if (markBMI > johnBMI) {
+  console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})`);
+} else {
+  console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})`);
+}
+
+const markMass2 = 95;
+const markHeight2 = 1.88;
+const johnMass2 = 85;
+const johnHeight2 = 1.76;
+const markBMI2 = markMass2 / markHeight2 ** 2;
+const johnBMI2 = johnMass2 / johnHeight2 ** 2;
+
+console.log(markBMI2, johnBMI2);
+
+let message;
+if (johnBMI2 > markBMI2) {
+  message = `John's BMI (${johnBMI2}) is higher than Mark's (${markBMI2})`;
+} else {
+  message = `Mark's BMI (${markBMI2}) is higher than John's (${johnBMI2})`;
+}
+
+console.log(message);
--- script.js
+++ script.js
@@ -84,4 +84,107 @@ console.log(x, y);
 
 const averageAge = (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah, averageAge);
+
+const firstName = "Jonas";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2037;
+
+const jonas =
+  "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+
+console.log("String with \n\
+multiple \n\
+lines");
+
+console.log(`String
+multiple
+lines`);
+
+const age = 15;
+
+if (age >= 18) {
+  console.log("Sarah can start driving license 🚗");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait antoher ${yearsLeft} years :)`);
+}
+
+const birthYear = 2012;
+
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
+
+// 20 Type Conversion and Coercion
+// Conversion is manually converted.
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Jonas"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// Coercion is automatically converted.
+console.log("I am " + 23 + " years old.");
+console.log("23" - "10" - 3); // logical operators *, , / converts strings to #s
+console.log("23" * "2");
+
+// 21 Truthy and falsy values
+// 5 falsy values: 0, ' ', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+
+const money = 100;
+if (money) {
+  console.log(`Don't spend it all :)`);
+} else {
+  console.log(`You should get a job!`);
+}
+
+let height;
+if (height) {
+  console.log(`Yay! Height is defined`);
+} else {
+  console.log("Height is UNDEFINED");
+}
 */
+
+// 22 Equality Operators
+// === strict equality operator doesn't use type of coersion.
+const age = "18";
+if (age === 18) console.log(`You just became an adult :D (strict)`);
+
+// == loose equality operator uses type of coersion.
+if (age == 18) console.log(`You just became an adult :D (loose)`);
+
+const favourite = Number(prompt(`What is your favorite number?`));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+  console.log(`Cool! 23 is an amazing number!`);
+} else if (favourite === 7) {
+  console.log(`7 is also a cool number`);
+} else {
+  console.log(`Number is not 23 or 7`);
+}
+
+// different operator !== strict != loose
+if (favourite !== 23) console.log(`Why not 23?`);
